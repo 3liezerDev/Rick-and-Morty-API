e84f545ccc2fd30dc7cff4a1272ecb21226afbb3
3liezerDev <3dgar3liezer@gmail.com>
2024-02-12 23:29:12 +0100

    consumir api de rick and morty

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-fetch('https://rickandmortyapi.com/api/character')
+fetch('https://rickandmortyapi.com/api/character/')
     .then(response => {
         if (!response.ok) {
             throw new Error(`Error en la solicitud: ${response.status}`);
